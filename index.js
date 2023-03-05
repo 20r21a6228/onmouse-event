6fa0b03dcc13a23588f98a076f2c0edb35941087
Sri Ram Charan marri <93866056+20r21a6228@users.noreply.github.com>
2023-03-06 01:27:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeclr(id,clr){
+    document.getElementById(id).style.borderColor=clr;
+
+}
+function changeclr1(id,clr){
+    document.getElementById(id).style.backgroundColor=clr;
+
+}
+function size(id,s){
+    document.getElementById(id).style.fontSize=s;
+}
